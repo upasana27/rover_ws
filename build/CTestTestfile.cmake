a3071c48231626d7681b2f85f00d7f58765433f2
upasana <upasana_biswas@ymail.com>
2018-06-22 20:52:51 +0530

    deleted build and devel

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hit_ter/rover_ws/src
-# Build directory: /home/hit_ter/rover_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(robotic_arm)
--- build/robotic_arm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hit_ter/rover_ws/src/robotic_arm
-# Build directory: /home/hit_ter/rover_ws/build/robotic_arm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
